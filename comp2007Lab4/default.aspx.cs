089683bb8fecec9a474d64c96c803bf8b4a9a782
Jeff Geear <jmgeear@gmail.com>
2015-07-22 13:17:54 -0400

    final

--- comp2007Lab4/default.aspx.cs
+++ comp2007Lab4/default.aspx.cs
@@ -9,10 +9,6 @@ namespace comp2007Lab4
 {
     public partial class _default : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            throw new InvalidOperationException("An InvalidOperationException " +
-      "occurred in the Page_Load handler on the Default.aspx page.");
-        }
+
     }
 }
